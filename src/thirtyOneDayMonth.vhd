9c9953c5e9c9ac508ac0582ddc7b5ad8b2eac1c4
xveqc3204 <c27john.alves@afacademy.af.edu>
2025-02-10 23:56:44 -0700

    ECE 281 Lab 1 Final Submission

--- src/thirtyOneDayMonth.vhd
+++ src/thirtyOneDayMonth.vhd
@@ -11,7 +11,7 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth.vhd
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
+--| AUTHOR(S)     : Capt Dan Johnson, John Alves
 --| CREATED       : 12/12/2019 Last Modified 06/24/2020
 --| DESCRIPTION   :  This file implements the thirtyOneDaMonth lab.  Using a 4
 --| switch input, the circuit will light up whenever the 4 switches represent a
@@ -56,24 +56,32 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
+	o_Y : out std_logic -- output
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
-	--signals internal to the architecture are declared and initialized such as w_sel
-  
+    signal w_sel : std_logic_vector (2 downto 0); -- MUX sel  
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
 	--finish assigning signals
-	
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+    o_Y <= i_D when (w_sel = "000") else
+           i_D when (w_sel = "001") else
+           i_D when (w_sel = "010") else
+           i_D when (w_sel = "011") else
+           NOT i_D when (w_sel = "100") else
+           NOT i_D when (w_sel = "101") else
+           NOT i_D when (w_sel = "110") else
+           NOT i_D when (w_sel = "111") else
+           '0';
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
--- src/thirtyOneDayMonth_tb.vhd
+++ src/thirtyOneDayMonth_tb.vhd
@@ -93,8 +93,35 @@ begin
 		w_sw <= x"0"; wait for 10 ns;
             assert w_Y = '0' report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jan" severity failure;   
-
+            assert w_Y = '1' report "error on Jan" severity failure; 
+		w_sw <= x"2"; wait for 10 ns;
+            assert w_Y = '0' report "error on Feb" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_Y = '1' report "error on Mar" severity failure;
+		w_sw <= x"4"; wait for 10 ns;
+            assert w_Y = '0' report "error on Apr" severity failure;
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_Y = '1' report "error on May" severity failure; 
+		w_sw <= x"6"; wait for 10 ns;
+            assert w_Y = '0' report "error on Jun" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_Y = '1' report "error on Jul" severity failure;  
+  		w_sw <= x"8"; wait for 10 ns;
+            assert w_Y = '1' report "error on Aug" severity failure;
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_Y = '0' report "error on Sep" severity failure; 
+		w_sw <= x"A"; wait for 10 ns;
+            assert w_Y = '1' report "error on Oct" severity failure;
+        w_sw <= x"B"; wait for 10 ns;
+            assert w_Y = '0' report "error on Nov" severity failure;
+		w_sw <= x"C"; wait for 10 ns;
+            assert w_Y = '1' report "error on Dec" severity failure;
+		w_sw <= x"D"; wait for 10 ns;
+            assert w_Y = '0' report "error on x1" severity failure; 
+		w_sw <= x"E"; wait for 10 ns;
+            assert w_Y = '1' report "error on x2" severity failure; 
+		w_sw <= x"F"; wait for 10 ns;
+            assert w_Y = '0' report "error on x3" severity failure; 
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
